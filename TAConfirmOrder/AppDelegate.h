386085f913d063c0967d069cb2ee21fc07645c52
15370989688 <15370989688@163.com>
2016-12-25 17:24:55 +0800

    Initial Commit

---
+++ TAConfirmOrder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TAConfirmOrder
+//
+//  Created by Charon on 16/12/25.
+//  Copyright © 2016年 Charon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
